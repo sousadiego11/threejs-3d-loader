2b98c50bae88d947e3f5647c327e374965d5f1da
Diego Sousa <diegointeriores11@gmail.com>
2022-11-25 16:40:07 -0300

    refactor: move files inside another

--- src/domCreators/createCadastroActions.js
+++ src/domCreators/createCadastroActions.js
@@ -1,5 +1,33 @@
-import { createRotateButton } from './createRotateButton';
-import { createUploadInput } from './createUploadInput';
+import { sceneHandler } from "../SceneHandler";
+import { makeObjectLoader } from '../factories';
+
+function createRotateButton(container) {
+    const rotateButton = document.createElement('button');
+    rotateButton.className = 'rotate';
+    rotateButton.innerText = 'Rotação automática';
+
+    container.appendChild(rotateButton);
+    document.addEventListener('click', (e) => {
+        if (e.target.className === 'rotate') {
+            sceneHandler.orbitControls.autoRotate = !sceneHandler.orbitControls.autoRotate;
+        }
+    });
+}
+
+ function createUploadInput(container) {
+    const inputUpload = document.createElement('input');
+    inputUpload.type = 'file';
+    inputUpload.className = 'upload';
+    inputUpload.multiple = true;
+
+    container.appendChild(inputUpload);
+    document.addEventListener('change', (e) => {
+        if (e.target.className === 'upload') {
+            const objectLoader = makeObjectLoader(document.querySelector('.upload').files);
+            objectLoader.load();
+        }
+    });
+}
 
 export function createCadastroActions() {
     const container = document.querySelector('.actions');
