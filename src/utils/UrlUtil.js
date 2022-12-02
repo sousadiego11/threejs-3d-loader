c7e14290b8841ffb2cd2a884ca18a19526ad9378
Diego Sousa <diegointeriores11@gmail.com>
2022-12-02 11:30:53 -0300

    fix: change remote url

--- src/utils/UrlUtil.js
+++ src/utils/UrlUtil.js
@@ -5,7 +5,7 @@ export class UrlUtil {
     }
 
     static getRemoteUrl() {
-        return process.env.REMOTE_API
+        return 'https://threed-loader-api.onrender.com:8080'
     }
 
     static getObjectUrl(object) {
